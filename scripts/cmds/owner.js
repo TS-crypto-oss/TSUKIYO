c4a767d06d777081f6f701704c47c10fc889bfc7
xxx-bot-crypto <cybertxt99@gmail.com>
2025-12-20 13:20:32 +0600

    Create owner.js

---
+++ scripts/cmds/owner.js
@@ -0,0 +1,10 @@
+module.exports = {
+  config: {
+    name: "owner",
+    category: "info",
+    role: 0
+  },
+  onStart: async ({ message }) => {
+    message.reply("👑 Bot owner: Sanjida Snigdha");
+  }
+};
